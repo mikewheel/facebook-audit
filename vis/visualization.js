95a5d43b2d3967c6da12e5a2f432872d710d0cb7
Sam Xifaras <s.xifaras999@gmail.com>
2019-02-10 09:05:39 -0500

    Fixed bar charts

--- vis/visualization.js
+++ vis/visualization.js
@@ -162,7 +162,7 @@ let exampleData = [
 function render() {
 
   // Render
-  let svg = ordinalBarChart(
+  /*let svg = ordinalBarChart(
       exampleData,
       "year",
       "numFriends",
@@ -172,7 +172,33 @@ function render() {
       titleY="",
       legend = false);
   document.getElementById("insert").appendChild(svg); // This div must also be classed with svg-container
+*/
 
+  let svg = ordinalBarChart([{
+        "status": "Accepted",
+        "count": parsedData["events"]["accepted"].length
+      },
+        {
+          "status": "Declined",
+          "count": parsedData["events"]["declined"].length
+        },
+        {
+          "status": "Hosted",
+          "count": parsedData["events"]["hosted"].length
+        },
+        {
+          "status": "Interested",
+          "count": parsedData["events"]["interested"].length
+        }],
+      "status",
+      "count",
+      FAKEBOOKBLUE,
+      "",
+      "",
+      "",
+      false);
+
+  document.getElementById("insert").appendChild(svg);
 
 /*
   let svg = addBorder(bigStatistic("NUMBER OF ADVERTISERS TARGETING YOU", 1040, "#4267b2", margin, 600, 100));
